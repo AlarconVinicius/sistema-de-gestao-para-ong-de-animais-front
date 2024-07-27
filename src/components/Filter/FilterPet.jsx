85ab3daba1073e29316660c49b5383854e64ac61
AlarconVinicius <alarcon.pqd74@gmail.com>
2024-07-27 19:56:22 -0300

    feat: Implementar filtro de pet

--- src/components/Filter/FilterPet.jsx
+++ src/components/Filter/FilterPet.jsx
@@ -1,23 +1,24 @@
 import React from 'react';
-import { Select, TextInput, Button } from 'flowbite-react';
+import PropTypes from 'prop-types';
+import { Select, TextInput } from 'flowbite-react';
 import { BiSearch } from 'react-icons/bi';
 
-const FilterPet = () => {
+const FilterPet = ({ handleFilterChange }) => {
     return (
-        <div className="lg:mx-14 bg-white p-4 rounded-lg shadow-md">
+        <div className="bg-white p-4 rounded-lg shadow-md">
             <div className="grid grid-cols-1 md:grid-cols-3 gap-4 mb-4">
-                <Select id="species" required>
+                <Select id="species" className='items-center' required onChange={handleFilterChange}>
                     <option>Todas as Espécies</option>
                     <option>Cachorro</option>
                     <option>Gato</option>
                     <option>Outros</option>
                 </Select>
-                <Select id="sex" required>
+                <Select id="sex" className='items-center' required onChange={handleFilterChange}>
                     <option>Todos os Sexos</option>
                     <option>Macho</option>
                     <option>Fêmea</option>
                 </Select>
-                <Select id="size" required>
+                <Select id="size" className='items-center' required onChange={handleFilterChange}>
                     <option>Todos os Portes</option>
                     <option>Pequeno</option>
                     <option>Médio</option>
@@ -25,32 +26,32 @@ const FilterPet = () => {
                 </Select>
             </div>
             <div className="grid grid-cols-1 md:grid-cols-12 gap-4 mb-4">
-                <Select id="state" className='md:col-span-3' required>
+                <Select id="state" className='md:col-span-3 items-center' required onChange={handleFilterChange}>
                     <option>Todos os Estados</option>
-                    <option>São Paulo</option>
-                    <option>Rio de Janeiro</option>
-                    <option>Minas Gerais</option>
-                    <option>Bahia</option>
-                    {/* Adicione mais estados conforme necessário */}
+                    <option>SP</option>
+                    <option>RJ</option>
+                    <option>MG</option>
+                    <option>BH</option>
                 </Select>
-                <Select id="city" className='md:col-span-3' required>
+                <Select id="city" className='md:col-span-3 items-center' required onChange={handleFilterChange}>
                     <option>Todas as Cidades</option>
                     <option>São Paulo</option>
                     <option>Rio de Janeiro</option>
                     <option>Belo Horizonte</option>
                     <option>Salvador</option>
-                    {/* Adicione mais cidades conforme necessário */}
                 </Select>
                 <div className="md:col-span-6 flex items-center space-x-4 w-auto">
-                    <TextInput className='w-full' id="name" placeholder="Nome do amigo" />
-                    <button className="btn-primary bg-primaryPurple border-none w-72 flex items-center justify-center">
+                    <TextInput className='w-full' id="name" placeholder="Nome do amigo" onChange={handleFilterChange} />
+                    <button className="btn-primary w-72 flex items-center justify-center">
                         <BiSearch className="mr-2" />
                         Buscar
                     </button>
                 </div>
             </div>
         </div>
-  )
+    );
 }
-
-export default FilterPet
+FilterPet.propTypes = {
+    handleFilterChange: PropTypes.func.isRequired
+};
+export default FilterPet;
--- src/routes/QueroAdotar.jsx
+++ src/routes/QueroAdotar.jsx
@@ -6,27 +6,34 @@ import CardPet from '../components/Card/CardPet';
 
 const QueroAdotar = () => {
     const [visibleCount, setVisibleCount] = useState(6);
+    const [speciesFilter, setSpeciesFilter] = useState('Todas as Espécies');
+    const [sexFilter, setSexFilter] = useState('Todos os Sexos');
+    const [sizeFilter, setSizeFilter] = useState('Todos os Portes');
+    const [stateFilter, setStateFilter] = useState('Todos os Estados');
+    const [cityFilter, setCityFilter] = useState('Todas as Cidades');
+    const [nameFilter, setNameFilter] = useState('');
 
     const breadcrumbItems = [
         { label: "Início", href: "/" },
         { label: "Quero adotar" }
     ];
+
     const allPets = [
-        {id: 1, nome: "Acerola", sexo: 0, ong: "ACASA - ASSOCIACAO COM AMOR SEM ABANDONO", endereco: "SAO BERNARDO DO CAMPO, SP", image: "/src/assets/cardImage.png"},
-        {id: 2, nome: "Amelia", sexo: 1, ong: "UNIÃO DE PROTEÇÃO ANIMAL DA BAHIA - UPABA", endereco: "SALVADOR, BA", image: "/src/assets/cardImage.png"},
-        {id: 3, nome: "ANAÍTA", sexo: 1, ong: "ACASA - ASSOCIACAO COM AMOR SEM ABANDONO", endereco: "SAO PAULO, SP", image: "/src/assets/cardImage.png"},
-        {id: 4, nome: "Helena", sexo: 1, ong: "Ferreira", endereco: "Cunha, AC", image: "/src/assets/cardImage.png"},
-        {id: 5, nome: "João Guilherme", sexo: 0, ong: "Peixoto Souza S.A.", endereco: "Moreira, SC", image: "/src/assets/cardImage.png"},
-        {id: 6, nome: "Mirella", sexo: 0, ong: "da Paz", endereco: "Oliveira da Prata, SC", image: "/src/assets/cardImage.png"},
-        {id: 7, nome: "Bianca", sexo: 0, ong: "da Rocha", endereco: "Carvalho do Oeste, PE", image: "/src/assets/cardImage.png"},
-        {id: 8, nome: "Otávio", sexo: 0, ong: "Monteiro", endereco: "Dias do Campo, SE", image: "/src/assets/cardImage.png"},
-        {id: 9, nome: "Beatriz", sexo: 1, ong: "Cardoso", endereco: "Novaes, RJ", image: "/src/assets/cardImage.png"},
-        {id: 10, nome: "Bruno", sexo: 0, ong: "Carvalho - EI", endereco: "Lopes, CE", image: "/src/assets/cardImage.png"},
-        {id: 11, nome: "Antônio", sexo: 1, ong: "da Mata Aragão S.A.", endereco: "Rodrigues, RO", image: "/src/assets/cardImage.png"},
-        {id: 12, nome: "Eloah", sexo: 1, ong: "Farias - EI", endereco: "Cunha, MT", image: "/src/assets/cardImage.png"},
-        {id: 13, nome: "Benício", sexo: 1, ong: "Peixoto S/A", endereco: "Martins das Pedras, AM", image: "/src/assets/cardImage.png"},
-        {id: 14, nome: "Lorenzo", sexo: 0, ong: "da Rosa", endereco: "Almeida, PB", image: "/src/assets/cardImage.png"},
-        {id: 15, nome: "Alícia", sexo: 0, ong: "da Mata", endereco: "Sales, AL", image: "/src/assets/cardImage.png"}
+        {id: 1, nome: "Acerola", sexo: 0, especie: "Cachorro", porte: "Pequeno", ong: "ACASA - ASSOCIACAO COM AMOR SEM ABANDONO", endereco: "SAO BERNARDO DO CAMPO, SP", estado: "SP", cidade: "São Bernardo do Campo", image: "/src/assets/cardImage.png"},
+        {id: 2, nome: "Amelia", sexo: 1, especie: "Gato", porte: "Médio", ong: "UNIÃO DE PROTEÇÃO ANIMAL DA BAHIA - UPABA", endereco: "SALVADOR, BA", estado: "BA", cidade: "Salvador", image: "/src/assets/cardImage.png"},
+        {id: 3, nome: "ANAÍTA", sexo: 1, especie: "Cachorro", porte: "Grande", ong: "ACASA - ASSOCIACAO COM AMOR SEM ABANDONO", endereco: "SAO PAULO, SP", estado: "SP", cidade: "São Paulo", image: "/src/assets/cardImage.png"},
+        {id: 4, nome: "Helena", sexo: 1, especie: "Gato", porte: "Pequeno", ong: "Ferreira", endereco: "Cunha, AC", estado: "AC", cidade: "Cunha", image: "/src/assets/cardImage.png"},
+        {id: 5, nome: "João Guilherme", sexo: 0, especie: "Cachorro", porte: "Grande", ong: "Peixoto Souza S.A.", endereco: "Moreira, SC", estado: "SC", cidade: "Moreira", image: "/src/assets/cardImage.png"},
+        {id: 6, nome: "Mirella", sexo: 0, especie: "Gato", porte: "Médio", ong: "da Paz", endereco: "Oliveira da Prata, SC", estado: "SC", cidade: "Oliveira da Prata", image: "/src/assets/cardImage.png"},
+        {id: 7, nome: "Bianca", sexo: 0, especie: "Cachorro", porte: "Pequeno", ong: "da Rocha", endereco: "Carvalho do Oeste, PE", estado: "PE", cidade: "Carvalho do Oeste", image: "/src/assets/cardImage.png"},
+        {id: 8, nome: "Otávio", sexo: 0, especie: "Gato", porte: "Grande", ong: "Monteiro", endereco: "Dias do Campo, SE", estado: "SE", cidade: "Dias do Campo", image: "/src/assets/cardImage.png"},
+        {id: 9, nome: "Beatriz", sexo: 1, especie: "Cachorro", porte: "Médio", ong: "Cardoso", endereco: "Novaes, RJ", estado: "RJ", cidade: "Novaes", image: "/src/assets/cardImage.png"},
+        {id: 10, nome: "Bruno", sexo: 0, especie: "Gato", porte: "Pequeno", ong: "Carvalho - EI", endereco: "Lopes, CE", estado: "CE", cidade: "Lopes", image: "/src/assets/cardImage.png"},
+        {id: 11, nome: "Antônio", sexo: 1, especie: "Cachorro", porte: "Grande", ong: "da Mata Aragão S.A.", endereco: "Rodrigues, RO", estado: "RO", cidade: "Rodrigues", image: "/src/assets/cardImage.png"},
+        {id: 12, nome: "Eloah", sexo: 1, especie: "Gato", porte: "Médio", ong: "Farias - EI", endereco: "Cunha, MT", estado: "MT", cidade: "Cunha", image: "/src/assets/cardImage.png"},
+        {id: 13, nome: "Benício", sexo: 1, especie: "Cachorro", porte: "Pequeno", ong: "Peixoto S/A", endereco: "Martins das Pedras, AM", estado: "AM", cidade: "Martins das Pedras", image: "/src/assets/cardImage.png"},
+        {id: 14, nome: "Lorenzo", sexo: 0, especie: "Gato", porte: "Grande", ong: "da Rosa", endereco: "Almeida, PB", estado: "PB", cidade: "Almeida", image: "/src/assets/cardImage.png"},
+        {id: 15, nome: "Alícia", sexo: 0, especie: "Cachorro", porte: "Médio", ong: "da Mata", endereco: "Sales, AL", estado: "AL", cidade: "Sales", image: "/src/assets/cardImage.png"}
     ];
     
 
@@ -34,25 +41,65 @@ const QueroAdotar = () => {
         setVisibleCount(prevCount => prevCount + 6);
     }
 
+    const handleFilterChange = (e) => {
+        const { id, value } = e.target;
+        switch (id) {
+            case 'species':
+                setSpeciesFilter(value);
+                break;
+            case 'sex':
+                setSexFilter(value);
+                break;
+            case 'size':
+                setSizeFilter(value);
+                break;
+            case 'state':
+                setStateFilter(value);
+                break;
+            case 'city':
+                setCityFilter(value);
+                break;
+            case 'name':
+                setNameFilter(value);
+                break;
+            default:
+                break;
+        }
+    };
+
+    const filteredPets = allPets.filter(pet => {
+        return (
+            (speciesFilter === 'Todas as Espécies' || pet.especie === speciesFilter) &&
+            (sexFilter === 'Todos os Sexos' || (sexFilter === 'Macho' ? pet.sexo === 0 : pet.sexo === 1)) &&
+            (sizeFilter === 'Todos os Portes' || pet.porte === sizeFilter) &&
+            (stateFilter === 'Todos os Estados' || pet.endereco.includes(stateFilter)) &&
+            (cityFilter === 'Todas as Cidades' || pet.endereco.includes(cityFilter)) &&
+            (nameFilter === '' || pet.nome.toLowerCase().includes(nameFilter.toLowerCase()))
+        );
+    });
+
     return (
         <div className='bg-neutralWhite1'>
             <div><Header breadcrumbItems={breadcrumbItems} /></div>
             <div className='md:px14 px-4 py-16 max-w-screen-xl mx-auto' id='quero-adotar'>
                 <h1 className='mb-3 text-center text-[32px] font-Nunito font-extrabold text-primaryPurple'>Campanha de Adoção</h1>
-                <p  className='mb-16 text-center text-[20px] font-Nunito text-primaryPurple'><span className='font-bold'>
+                <p className='mb-16 text-center text-[20px] font-Nunito text-primaryPurple'><span className='font-bold'>
                     Uma seleção especial de peludinhos que buscam um lar para chamar de seu.</span><br />
                     Não encontrou seu pet aqui ainda? Não se preocupe! Entre em contato com uma das <Link to="/ongs-protetores" className='font-bold'>ONGs parceiras</Link> para obter mais informações sobre outros pets disponíveis para adoção.<br />
                     Vamos juntos transformar a vida desses peludinhos com amor e carinho. Adote e faça a diferença!
                 </p>
-                <FilterPet />
+                <FilterPet handleFilterChange={handleFilterChange} />
                 <div className='mt-16 justify-items-center grid lg:grid-cols-3 md:grid-cols-3 grid-cols-1 gap-5 overflow-x-hidden'>
                     {
-                        allPets.slice(0, visibleCount).map(pet => 
-                            <CardPet key={pet.id} pet={pet} />
-                        )
-                    }                
+                        filteredPets.length === 0 ? (
+                            <p className="text-center text-neutralGrey2 font-bold">Nenhum pet encontrado</p>
+                        ) : (
+                            filteredPets.slice(0, visibleCount).map(pet => 
+                                <CardPet key={pet.id} pet={pet} />
+                            )
+                        )}                
                 </div>
-                {visibleCount < allPets.length && (
+                {visibleCount < filteredPets.length && (
                     <div className='flex justify-center mt-8'>
                         <button onClick={handleLoadMore} className='text-secondaryOrange text-medium btn-primary bg-transparent border-2 border-secondaryOrange'>Carregar mais</button>
                     </div>
